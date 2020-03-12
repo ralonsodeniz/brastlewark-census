4cd8625fa04fd3b27c628f59786df8fdfaf0eea7
ralonsodeniz <ralonsodeniz@gmail.com>
2020-03-12 17:53:23 +0100

    Added SearchBox component

---
+++ src/components/SearchBox/SearchBox.styles.jsx
@@ -0,0 +1,72 @@
+import styled, { css } from 'styled-components';
+
+import * as variables from '../../style/variables';
+import mediaQueryHelper from '../../style/media-queries';
+
+const shrinkLabelStyles = css`
+  top: -0.5rem;
+  font-size: ${variables.smallFontSize};
+  background-image: none;
+  -webkit-text-fill-color: ${variables.darkBlue};
+
+  ${mediaQueryHelper(
+    'phone',
+    css`
+      top: -0.4rem;
+    `
+  )};
+`;
+
+export const SearchBoxContainer = styled.div`
+  padding: 0.5rem;
+  position: relative;
+  align-self: center;
+`;
+
+export const SearchBoxInput = styled.input`
+  padding: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${variables.darkBlue};
+  display: block;
+  /* background: none; */
+  border-radius: 5px;
+  background-color: ${variables.darkBlue};
+  font-size: ${variables.defaultFontSize};
+  color: ${variables.whiteColor};
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus ~ label {
+    ${shrinkLabelStyles}
+  }
+
+  ${mediaQueryHelper(
+    'phone',
+    css`
+      padding: 0.35rem;
+    `
+  )};
+`;
+
+export const SearchBoxLabel = styled.label`
+  font-size: ${variables.defaultFontSize};
+  position: absolute;
+  pointer-events: none;
+  left: 1rem;
+  top: 17.5px;
+  transition: 300ms ease all;
+  z-index: 9;
+  /* color: ${variables.darkBlue}; */
+  ${variables.gradientTextStyle}
+  ${({ shrink }) => (shrink ? shrinkLabelStyles : '')};
+
+  ${mediaQueryHelper(
+    'phone',
+    css`
+      top: 15px;
+    `
+  )};
+`;
