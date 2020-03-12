1f4233063eb3e4c10d2167ec21fb9041898323aa
ralonsodeniz <ralonsodeniz@gmail.com>
2020-03-12 22:21:59 +0100

    Added Footer component and updated global style for footer styiling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+import { FooterContainer, FooterText, FooterLink } from './Footer.styles';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>
+        {' '}
+        © {new Date().getFullYear()} - Developed by{' '}
+        <FooterLink
+          href="https://github.com/ralonsodeniz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Raúl Alonso Déniz
+        </FooterLink>
+      </FooterText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
