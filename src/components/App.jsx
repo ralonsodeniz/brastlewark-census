7361e6ccbe6502644c72cd70bbffbd77e963f661
ralonsodeniz <ralonsodeniz@gmail.com>
2020-03-13 11:25:38 +0100

    Added Spinner. Added Citizens page to App routes. Added modal to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector, shallowEqual } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectShowModal } from '../redux/selectors/modalSelectors';
 
 import NavBar from './NavBar/NavBar';
+import InnerModal from './Modal/InnerModal';
+import Modal from './Modal/Modal';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import Footer from './Footer/Footer';
+import Spinner from './Spinner/Spinner';
 
 import HomePage from '../pages/Home/Home';
 
 import GlobalStyle from '../global.styles';
 import AppContainer from './App.styles';
 
+const Citizens = lazy(() => import('../pages/Citizens/Citizens'));
+
+const selectAppData = createStructuredSelector({
+  showModal: selectShowModal,
+});
+
 const App = () => {
+  const appData = useSelector(selectAppData, shallowEqual);
+  const { showModal } = appData;
+
   return (
     <>
       <GlobalStyle />
@@ -18,9 +34,17 @@ const App = () => {
       <AppContainer>
         <ErrorBoundary>
           <Switch>
-            <Route exact path="/" component={HomePage} />
+            <Suspense fallback={<Spinner />}>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/citizens" component={Citizens} />
+            </Suspense>
           </Switch>
         </ErrorBoundary>
+        {showModal && (
+          <Modal>
+            <InnerModal />
+          </Modal>
+        )}
       </AppContainer>
       <Footer />
     </>
---
+++ src/components/Spinner/Spinner.styles.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+import * as variables from '../../style/variables';
+
+export const SpinnerOverlay = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SpinnerContainer = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 3px solid ${variables.primaryColor};
+  border-radius: 50%;
+  border-top-color: ${variables.darkGrey};
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+`;
